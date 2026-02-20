d8f9d775aa049ab18b643de4969bd0ad3972c1b6
Marlene Terra <crocpeigne@protonmail.com>
2026-02-20 03:17:33 +0100

    ajout arriere-plan

---
+++ paysage/arriere-plan.py
@@ -0,0 +1,20 @@
+from gturtle import *
+
+def arriere_plan(base,hauteur_1,hauteur_2, couleur_1, couleur_2) :
+    hideTurtle()
+    setPos(- (base / 2 - hauteur_1 / 2),- hauteur_1 / 2)
+    setHeading(90)
+    setPenColor(couleur_1)
+    setPenWidth(hauteur_1)
+    forward(base - hauteur_1)
+
+    setPos(- (base / 2 - hauteur_2 / 2), hauteur_2 / 2)
+    setHeading(90)
+    setPenColor(couleur_2)
+    setPenWidth(hauteur_2)
+    forward(base - hauteur_2)
+
+
+
+if __name__ == "__main__":
+    arriere_plan(600,200,150, "SeaGreen", "LightCyan")

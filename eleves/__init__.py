9ee2b421f9cd1044e81b60bce154e63629173ac0
Marlene Terra <crocpeigne@protonmail.com>
2026-02-20 02:15:30 +0100

    ajout init

---
+++ eleves/__init__.py
@@ -0,0 +1,5 @@
+# Package des contributions élèves (eleves)
+# Ne pas importer automatiquement les modules ici pour éviter d'exécuter du dessin au moment de l'import
+# Les modules élèves seront importés explicitement, par exemple :
+# from eleves.moulin_anne import moulin
+__all__ = []
